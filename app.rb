ba11421835992e4da180d6b946d2e6569037c807
tiffanynuugen <tiffanynuugen@gmail.com>
2018-09-19 15:18:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		@weekday = Date.today.strftime("%A")
+		@month = Date.today.strftime("%B")
+		@day = Time.now.asctime.split(" ")[2]
+		@year = Time.now.asctime.split(" ")[4]
+		erb :date
+	end
 
 end
